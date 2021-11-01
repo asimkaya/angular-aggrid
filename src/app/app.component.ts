dffb1f19821fd552f865fdfc187e9ef3c965d121
asimkaya <asim.emre.kaya@gmail.com>
2021-11-01 11:55:42 +0300

    table ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component, VERSION } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  columnDefs: ColDef[] = [
+    { field: 'make', sortable: true, filter: true },
+    { field: 'model', sortable: true },
+    { field: 'price', sortable: true },
+  ];
+
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+  ];
 }
